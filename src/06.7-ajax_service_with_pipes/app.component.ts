914c2667c426a3b714fa73e6c8d0ea36e0b11bf4
sureshcskkumar <sureshcskkumar@gmail.com>
2022-05-04 10:29:40 +0530

    ajax with pipes

--- src/06.7-ajax_service_with_pipes/app.component.ts
+++ src/06.7-ajax_service_with_pipes/app.component.ts
@@ -1,41 +1,23 @@
 import { Component } from "@angular/core";
+import { Address, User } from "./models/user.model";
 import { UserService } from "./services/user.service";
 
 
-class Address {
-
-    constructor(
-        public street: number,
-        public city: string,
-        public zipcode: string
-    ) { }
-
-}
-    
-class User {
-    constructor(
-        public id: number,
-        public username: string,
-        public email: string,
-        public address: Address,
-        public dob: Date
-    ) { }
-}
 @Component({
     selector: "app-root",
     templateUrl: `app.component.html`,
     styleUrls: ["app.component.scss"],
     providers: []
 })
-export class AppComponent{
-    // user: User[] = [];
+export class AppComponent {
+    // user:User[] = [];
     users:Array<User> = [];
 
-    constructor(private userService:UserService) {
+    constructor(private userService:UserService){
         this.getAllUsers();
     }
 
-    getAllUsers() {
+    getAllUsers(){
         this.userService.fetchAllUsers().subscribe({
             next: (res:any)=>{
                 console.log("Response from database:")
@@ -43,13 +25,41 @@ export class AppComponent{
 
                 this.users = res;
 
-                res.forEach( (user:User) => {
+                res.forEach((user:User)=>{
                     user.dob = new Date("2017-06-15")
                 })
 
                 console.log(this.users)
+
             }
         })
     }
 
+
+    removeUser(id:number){
+        console.log("Remove user with id: "+id);
+        this.userService.removeUser(id).subscribe({
+            next: ()=>{
+                this.getAllUsers();
+            }
+        })
+    }
+    createNewUser(){
+        console.log("Creating new User");
+
+        let user = new User(9999, "9999", "9999@gmail.com", new Address("9999", "9999", "9999"), new Date());
+
+        this.userService.saveUser(user).subscribe({
+            next:(res:any)=>{
+                console.log("++++++++++++++++++++")
+                console.log("++++++++++++++++++++")
+                console.log("++++++++++++++++++++")
+                console.log("++++++++++++++++++++")
+                console.log("++++++++++++++++++++")
+                console.log(res);
+                this.getAllUsers();
+            }
+        })
+
+    }
 }
