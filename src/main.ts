fadf4779f203d1329d1ccf3da8c389547806e8ec
sureshcskkumar <sureshcskkumar@gmail.com>
2022-05-04 20:50:10 +0530

    Routing, SPA and Data sharing examples

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 // import { AppModule } from "./app/app.module";
-import { AppModule } from "./10-data-sharing-2/app.module";
+import { AppModule } from "./10-data-sharing-3/app.module";
 
 
 
